9cf6772ca3a023a492d45a16da2cb8ae82e532b5
Dylan Kehres <dkehres@mail.bradley.edu>
2020-04-25 13:32:43 -0400

    Folder restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Root from "./root";
-import NavBar from "./components/navbar";
+import Root from "./web/root";
+import NavBar from "./web/components/navbar";
 
 function App() {
   return (
