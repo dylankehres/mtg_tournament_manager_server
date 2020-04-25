e512683d3f3ec47b46a645cdfbc3060a872f3244
Dylan Kehres <dkehres@mail.bradley.edu>
2020-04-25 13:40:25 -0400

    Added spring initializer

---
+++ src/test/java/com/djk/tournament_manager/TournamentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djk.tournament_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
