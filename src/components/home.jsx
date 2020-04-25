52df1ea5151cce7254fe9b565285e5965f9f0a9e
Dylan Kehres <dkehres@mail.bradley.edu>
2020-04-25 10:07:42 -0400

    Removed rout comments

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 
-class Home extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="m-2">
-        <h2>Welcome to MTG Tournament Manager!</h2>
-        <Form>
-          <Button className="btn btn-primary m-2" href="/tournament">
-            Tournaments
-          </Button>
-        </Form>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className="m-2">
+      <h2>Welcome to MTG Tournament Manager!</h2>
+      <Form>
+        <Button className="btn btn-primary m-2" href="/tournament">
+          Tournaments
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Home;
